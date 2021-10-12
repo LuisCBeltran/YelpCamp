50468fda52e583e6b06f4bdb2e3255f3aa7dea7c
Luis C. Beltran <lcarlosbeltran@gmail.com>
2021-10-11 22:14:42 -0500

    mobile hamburger nav finished

---
+++ script.js
@@ -0,0 +1,35 @@
+// Close Aknowledgement
+
+function closeAcknowledgement() {
+    document.getElementById("acknowledgement").style.display = "none";
+}
+
+// Open Menu
+
+function openMenu() {
+    document.getElementById("navLinks").style.opacity = "1";
+    document.getElementById("navLinks").style.visibility = "visible";
+    document.getElementById("navButton").style.display = "none";
+    document.getElementById("navButtonOpen").style.display = "flex";
+}
+
+function closeMenu() {
+    document.getElementById("navLinks").style.opacity = "0";
+    document.getElementById("navLinks").style.visibility = "hidden";
+    document.getElementById("navButton").style.display = "flex";
+    document.getElementById("navButtonOpen").style.display = "none";
+}
+
+// Login
+
+function login() {
+    document.getElementById("navLinksLogin").style.display = "flex";
+    document.getElementById("navLinksLogout").style.display = "none";
+}
+
+// Logout
+
+function logout() {
+    document.getElementById("navLinksLogin").style.display = "none";
+    document.getElementById("navLinksLogout").style.display = "flex";
+}
